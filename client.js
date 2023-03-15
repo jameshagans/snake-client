5ceab3f10e7262dfd9c7e822137caa649ecfde3a
jameshagans <jamesy.h95@gmail.com>
2023-03-15 20:00:19 +0000

    adding client.js file that stores our server connect function and exported to play.js

---
+++ client.js
@@ -0,0 +1,20 @@
+const net = require("net"); 
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: '165.227.47.243',
+    port: 50541
+  });
+
+  //interpert incoming data as text
+  conn.setEncoding("utf-8")
+
+  conn.on("data", (data) => {
+  // code that does something when data sent by the server
+  console.log(data)
+});
+  
+  return conn;
+}
+
+module.exports = connect; 
--- play.js
+++ play.js
@@ -1,21 +1,6 @@
 const net = require("net"); 
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243',
-    port: 50541
-  });
-
-  //interpert incoming data as text
-  conn.setEncoding("utf-8")
-
-  conn.on("data", (data) => {
-  // code that does something when data sent by the server
-  console.log(data)
-});
-  
-  return conn;
-}
+const connect = require('./client.js'); 
 
 console.log('Connecting...'); 
 connect()
